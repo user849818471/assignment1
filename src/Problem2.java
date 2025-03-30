bcf3a2c3cd0acdc66e4580037b3a7d0a799a1520
user <user@user.com>
2025-03-30 19:42:39 +0500

    2

---
+++ src/Problem2.java
@@ -0,0 +1,26 @@
+/*
+  This method calculates the average of a given set of numbers.
+  It uses an iterative approach.
+  Time complexity: O(n), where n is the number of integers.
+  The algorithm iterates through all numbers once to compute the sum,
+  then performs a single division operation, resulting in linear time complexity.
+
+  @param n The number of integers in the input.
+  @return The average value of the given numbers.
+ */
+
+import java.util.Scanner;
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += scanner.nextInt();
+        }
+        double average = (double) sum / n;
+        System.out.println(average);
+
+        scanner.close();
+    }
+}
